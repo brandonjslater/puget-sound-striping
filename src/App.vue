c1429aa5bedcaa170cea87a0afbac83dce0b24cc
bs@nostromo <brandon.sltaer94@gmail.com>
2021-10-26 20:01:53 -0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="https://media.istockphoto.com/photos/teamwork-pavement-asphalt-road-marking-paint-and-striping-with-picture-id487565318?s=612x612">
+  <firstPage 
+    msg="Puget Sound Striping"
+    subMsg="Some Subtext"/>
+</template>
+
+<script>
+import firstPage from './components/firstPage.vue';
+
+export default {
+  name: 'application',
+  components: {
+    firstPage
+  }
+};
+</script>
+
+<style lang="less">
+img{
+  max-width:100%;
+}
+#app {
+  font-family: Tahoma;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/firstPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>{{ subMsg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'firstPage',
+  props: {
+    msg: String,
+    subMsg: String,
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
